ce592ea572a0c07f191ae5a56913b348446c7311
naikwadehimanshu <naikwadehimanshu007@gmail.com>
2026-04-27 16:02:57 +0530

    Resolved server issues

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const connectDB = require('./config/db');
 
 // Connect to MongoDB
@@ -23,7 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files (frontend)
-app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 // API Routes
 app.use('/api/auth', require('./routes/auth'));
@@ -37,11 +35,7 @@ app.get('/api/health', (req, res) => {
 });
 
 // Serve frontend for all non-API routes
-app.get('*', (req, res) => {
-  if (!req.path.startsWith('/api')) {
-    res.sendFile(path.join(__dirname, '../frontend/public/index.html'));
-  }
-});
+
 
 // Global error handler
 app.use((err, req, res, next) => {
